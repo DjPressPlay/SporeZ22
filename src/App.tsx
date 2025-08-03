c62a2312864f710713a6a20e5dff69c92e503cdd
Artworqq <keysub.kevin@gmail.com>
2025-08-02 23:46:38 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Spore’z Shortlink</h1>
+      <p>This is your Bitly clone homepage.</p>
+    </div>
+  );
+}
